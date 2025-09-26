74e1ed9321432d12b67253d747e38c12e08cc638
manh.nv2102 <manh.nv2102@gmail.com>
2025-09-27 00:13:19 +0700

    video 39

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,27 @@
 //const express = require("express");
 import express from "express";
-const app = express();
-const port = 8080;
+import "dotenv/config";
+import getConnection from "./config/database";
+import webRoutes from "./routes/web";
 
-app.get("/", (req, res) => {
-  res.send("Hello World! My name is Mạnh");
-});
+const app = express();
+const port = process.env.PORT || 8080;
+
+//config view engine
+app.set("view engine", "ejs");
+app.set("views", __dirname + "/views");
+
+//config req.body
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+//config statc:img,css,js
+app.use(express.static("public"));
+
+//config routes
+webRoutes(app);
+
+getConnection();
 
 app.listen(port, () => {
   console.log(`My app listening on port ${port}`);
---
+++ src/services/user.service.ts
@@ -0,0 +1,9 @@
+const handleCreateUser = (email: string, password: string) => {
+  // inser into database
+
+  //return result
+
+  console.log(">>> insert a new user");
+};
+
+export { handleCreateUser };
